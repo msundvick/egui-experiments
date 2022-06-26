84ba1537dfcb158502c502c525a121116dc524eb
msundvick <kynac1@gmail.com>
2022-06-26 05:38:34 +0000

    Deploying to gh-pages from @ msundvick/egui-experiments@5d30598897589f2fa1fec879fdb861975715f70d 🚀

---
+++ main.js
@@ -0,0 +1,27 @@
+import init, { start } from "./egui_experiments.js";
+try {
+  await init();
+} catch {
+  console.error("Failed to start: " + error);
+  document.getElementById("center_text").innerHTML = `
+                <p>
+                    An error occurred during loading:
+                </p>
+                <p style="font-family:Courier New">
+                    ${error}
+                </p>
+                <p style="font-size:14px">
+                    Make sure you use a modern browser with WebGL and WASM enabled.
+                </p>`;
+} finally {
+  console.debug("wasm loaded. starting app…");
+  start("the_canvas_id");
+  console.debug("app started.");
+  document.getElementById("center_text").remove();
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
